2fc378e41ef21f4b47c509300074a2765845bb02
CN_Sakura <XXF18374524605@outlook.com>
2025-11-24 20:57:10 +0800

    神の書项目启动

---
+++ web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from web import models
+
+# Create your views here.
+
+
+def trans(request):
+    return render(request,'index.html')
